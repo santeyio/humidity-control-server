018f9704f0f74f44890828e936e4a22f1f71bdc8
Caleb Hayashida <santeyio@gmail.com>
2023-11-07 12:45:28 -0500

    ui rehaul

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import json
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify
 
-app = Flask(__name__)
+#  CURRENT_READINGS_FILE_PATH = '/home/pi/Documents/hsys/humidity-control/current.txt'
+CURRENT_READINGS_FILE_PATH = '/home/caleb/dev/humidity-control-server/sample_current.txt'
 
-@app.route('/')
-def hello_world():
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+@app.route('/readings')
+def get_current_readings():
     data = None
-    with open('/home/pi/Documents/hsys/humidity-control/current.txt', 'r') as f:
+    with open(CURRENT_READINGS_FILE_PATH, 'r') as f:
         data = json.loads(f.read())
-    return render_template('index.html', temp=data['t'], humidity=data['h'], time=data['time'])
+    return jsonify({
+        'temp': data['t'],
+        'humidity': data['h'],
+        'read_time': data['time'],
+    })
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def hello_world(path):
+    return app.send_static_file('index.html')
